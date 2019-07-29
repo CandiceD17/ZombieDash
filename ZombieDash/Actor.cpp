bd82612093541efa289937e963174b59e80e082d
Candice Deng <47619020+CandiceD17@users.noreply.github.com>
2019-07-29 13:41:23 +0800

    Update Actor.cpp

--- ZombieDash/Actor.cpp
+++ ZombieDash/Actor.cpp
@@ -1,10 +1,6 @@
 #include "Actor.h"
 #include "StudentWorld.h"
 
-// Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
-
-//vaccine when zombie die
-
 void Penelope::doSomething(){
     if(StateInfection())
         addInfection();
